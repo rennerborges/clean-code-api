332da21b77b2482906adb109ed32d13abfba85ae
Renner Borges <rennerferreira23@gmail.com>
2021-04-20 10:00:36 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -11,17 +12,17 @@ describe('Account Mongo Repository', () => {
   test('Teste', async () => {
     expect(true).toBe(true)
   })
-//   test('Should returns an account on success', async () => {
-//     const sut = new AccountMongoRepository()
-//     const account = await sut.add({
-//       name: 'any_name',
-//       email: 'any_email@mail.com',
-//       password: 'any_password'
-//     })
-//     expect(account).toBeTruthy() // Verifica se o valor não é Null
-//     expect(account.id).toBeTruthy()
-//     expect(account.name).toBe('any_name')
-//     expect(account.email).toBe('email')
-//     expect(account.password).toBe('any_password')
-//   })
+  test('Should returns an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy() // Verifica se o valor não é Null
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
 })
